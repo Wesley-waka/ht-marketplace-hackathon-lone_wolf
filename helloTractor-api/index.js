3732368250998360a4a9b41a7480625f5ac07413
Wesley-waka <wesleywaka77@gmail.com>
2024-11-29 11:11:17 +0300

    pckg-lock

--- helloTractor-api/index.js
+++ helloTractor-api/index.js
@@ -60,7 +60,7 @@ app.get('/api', (req, res) => {
   res.send('Hello Tractor API is running....');
 });
 
-const PORT =  3000;
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
